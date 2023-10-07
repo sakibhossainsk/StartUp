2ca314187fd7c64d8daa26d6dd20b3e28b15c699
SakibHossain <136673238+sakibhossainsk@users.noreply.github.com>
2023-10-07 16:21:24 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//js 
+//scroll start
+let $button = $.backToTop({
+        backgroundColor: '#846990',
+        container: $('.inner'),
+        effect: 'spin-inverse',
+        position: 'bottom right',
+        scrollAnimation: 400,
+        theme: 'fawesome'
+    });
+    $button.resize(50, 50);
